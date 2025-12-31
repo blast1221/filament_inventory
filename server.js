360598682dd7af7172e21bb5b927ef47f33f29ef
Luis Garcia Jr <luis.garciajr90@yahoo.com>
2025-12-30 18:26:25 -0600

    Corrected item.color to reflect Supabase

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import fetch from "node-fetch";
+import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -7,31 +7,48 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const port = 3000;
 
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
 app.get("/inventory", async (req, res) => {
-    const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_NAME)}`;
+    const { data, error } = await supabase
+        .from('colors')
+        .select('*');
 
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+        if (error) {
+            return res.status(500).json({ error: error.message});
         }
-    });
-
-    const data = await response.json();
     
-    const formatted = data.records.map(record => ({
-        id: record.id,
-        color: record.fields.Color,
-        finish: record.fields.Finish,
-        description: record.fields.Description,
-        inStock: record.fields.inStock,
-        colorHex1: record.fields.ColorHex1,
-        colorHex2: record.fields.ColorHex2,
-        colorHex3: record.fields.ColorHex3
+    const formatted = data.map(item => ({
+        id: item.id,
+        color: item.color,
+        finish: item.finish,
+        description: item.description,
+        inStock: item.inStock,
+        colorHex1: item.colorHex1,
+        colorHex2: item.colorHex2,
+        colorHex3: item.colorHex3
     }));
+
     res.json(formatted);
+});
+
+app.post("/inventory", async (req, res) => {
+    const { name, finish, description, inStock, colorHex1 } = req.body;
+
+    const { data, error } = await supabase
+        .from('colors')
+        .insert([{ name, finish, description, inStock, colorHex1 }])
+        .select();
+
+    if (error) return res.status(500).json(error);
+    res.json(data);
 });
 
 app.listen(port, () => {
