ff086fff79915a4bf698fc4ca146392d7e042043
Luis Garcia Jr <luis.garciajr90@yahoo.com>
2025-12-30 23:32:20 -0600

    Updated server.js. Missing import path from path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import { fileURLToPath } from "url"; // New
 
 dotenv.config();
